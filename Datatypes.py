a0ae22e3947905369ee2c99ad9716e159e3a7556
Prerna <mhatreprerna800@gmail.com>
2026-04-29 12:51:09 +0530

    data types diff

--- Datatypes.py
+++ Datatypes.py
@@ -1,4 +1,4 @@
-
+'''
 # int
 age = 40
 print(age)
@@ -58,3 +58,74 @@ range(0, 5)
 <class 'range'>
 b'ABa'
 bytearray(b'ABa')
+
+
+name="bunny"
+name[3]="r"
+print(name)
+
+#  name[3]="r"
+   # ~~~~^^^
+#TypeError: 'str' object does not support item assignment
+
+#list
+list_1=["red",'green','white']
+print(list_1)
+list_1[2]= "black"
+print(list_1)
+
+['red', 'green', 'white']
+['red', 'green', 'black']
+
+///////////////////
+list_1=["red",'green','white']
+print(list_1)
+list_1.append( "black")
+print(list_1)
+['red', 'green', 'white']
+['red', 'green', 'white', 'black']
+////////////////////
+
+#tuple
+tup1=("rice","wheat")
+print(tup1)
+tup1[1]="jowar"
+print(tup1)
+, line 84, in <module>
+    tup1[1]="jowar"
+    ~~~~^^^
+TypeError: 'tuple' object does not support item assignment
+
+
+#set
+set1={1,2,3,5,3,}
+print(set1)
+#{1, 2, 3, 5}
+set1.add(4)
+print(set1)
+#{1, 2, 3, 4, 5}
+
+#dictionary
+disc1={'name':"James","age":30,"country":"USA"}
+print(disc1)
+disc1["country"]="Europe"
+print(disc1)
+{'name': 'James', 'age': 30, 'country': 'USA'}
+{'name': 'James', 'age': 30, 'country': 'Europe'}
+
+
+set1={2,  4,3,7,5}
+print(set1) #{2, 3, 4, 5, 7}
+'''
+
+set1={"b","d",'a'}
+print(set1)
+{'b', 'a', 'd'}
+
+{'a', 'b', 'd'}
+
+{'b', 'a', 'd'}
+
+What you put in set	Order behavior
+Small integers (like 1,2,3,4,5)	✅ Always in order
+Anything else (words, letters, big numbers)	❌ Random/Weird order
