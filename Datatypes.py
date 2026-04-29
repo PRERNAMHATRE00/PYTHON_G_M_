4a143838248326565a2c18d57869fde069408669
Prerna <mhatreprerna800@gmail.com>
2026-04-29 11:47:13 +0530

    data types

---
+++ Datatypes.py
@@ -0,0 +1,60 @@
+
+# int
+age = 40
+print(age)
+#float
+pie_ = 3.14
+print(pie_)
+# string 
+name= "Suga"
+print(name)
+#boolean
+is_adult= True
+print(is_adult)
+#list
+list1=[100,200,300]
+print(list1)
+print(type(list1))
+#dictionary
+dict1={'name':'Suga','age':32,}
+print(dict1)
+print(dict1['age'])
+#tuple
+tup=(20,30,40)
+print(tup)
+#set
+set1={1,2,3,4,5}
+print(set1)
+#none
+sibling= None
+print(sibling)
+#complex
+com = 1+2j
+print(com)
+#range
+num = range(0,5)
+print(num)
+print(type(num))
+#byte & bytearray
+byte= bytes([65,66,97])
+print(byte)
+byte1= bytearray([65,66,97])
+print(byte1)
+
+
+40
+3.14
+Suga
+True
+[100, 200, 300]
+<class 'list'>
+{'name': 'Suga', 'age': 32}
+32
+(20, 30, 40)
+{1, 2, 3, 4, 5}
+None
+(1+2j)
+range(0, 5)
+<class 'range'>
+b'ABa'
+bytearray(b'ABa')
