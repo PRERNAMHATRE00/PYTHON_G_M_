8523858c906a39191805fb3cd6dfc2db0aee0c25
Prerna <mhatreprerna800@gmail.com>
2026-04-29 06:04:44 +0530

    input variable

---
+++ input.py
@@ -0,0 +1,12 @@
+
+num = bool(input("Enter any Number : "))
+print(num)
+print(type(num))
+
+
+'''
+Enter any Number : 
+False
+<class 'bool'>
+
+'''
---
+++ variale.py
@@ -0,0 +1,33 @@
+var1=1
+print(var1)
+var_1=2
+print(var_1)
+_var1=3
+print(_var1)
+1var=4     #invalid decimal lteral '''
+print(1var)
+var 1=5   # invalid syntax
+
+print(var 1)
+
+var_var2=6
+print(var_var2)
+
+def=7 #invalid syntax
+print(def)
+deff=8
+print(deff)
+
+
+#case sensitive
+a=11
+A=32
+print(a)//11
+print(A)// 32
+
+
+
+
+
+
+
